dde7ed1e004aa9ec21aa01a88e46d98e97f94e33
Philipp Uthoff <philipp.uthoff@gmx.de>
2021-10-13 23:52:57 +0200

    Added user permission check

--- Controller/companyController.js
+++ Controller/companyController.js
@@ -1,8 +1,19 @@
 const db = require('../Script/db_connect');
 
-module.exports.contactData = function (companyId) {
-    var query = `SELECT CompanyName1, CompanyName2, Street, Postcode, City, Phone FROM [B_BaseData] WHERE Company = '${companyId}'`
-    return db.query(query).then(result => {
-        return result;
-    });
+module.exports.companyData = async function (req, res) {
+    const cId = req.params.cId;
+    const query = `SELECT CompanyName1, CompanyName2, Street, Postcode, City, Phone
+                   FROM [B_BaseData]
+                   WHERE Company = '${cId}'`;
+    const cData = await db.query(query);
+    res.send(cData[0]);
+}
+
+module.exports.specialTime = function (req, res) {
+    const typeKey = req.params.typeKey;
+    const cId = req.params.cId;
+    const query = `SELECT [Name]
+                   FROM [B_SpecialTime]
+                   WHERE [Company] = '${cId}' AND LanguageKey = 'deu' AND TypeKey = ${{typeKey}}`;
+    db.query(query);
 }
--- server.js
+++ server.js
@@ -12,18 +12,25 @@ const PORT = 8080;
 app.use(express.json())
 app.listen(PORT, () => console.log("Webserver started on http://localhost:" + process.env.APP_PORT));
 
-//Routes
+// Routes
 app.get('/company/:cId/login/:email', (req, res) => loginController.login(req, res));
-app.get('/company/:cId', authenticateToken, (req, res) => companyController.contactData(req.params.cId).then(result => res.send(result)));
-//res.status(404).send('not found message');
+app.post('/company/:cId/block/:email, ', (req, res) => loginController.blockAccount(req, res))
 
-//Verify
+app.get('/company/:cId', authenticateToken, (req, res) => companyController.companyData(req, res));
+app.get('/company/:cId/specialtime/:type', authenticateToken, (req, res) => companyController.specialTime(req, res));
+
+//Verify access-token
 function authenticateToken(req, res, next) {
     const token = req.headers['auth'];
-    if (token == null) return res.sendStatus(401);
+    if (token == null) return res.sendStatus(401).send('Missing access-token.');
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, content) => {
-        if (err) return res.sendStatus(403);
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, userData) => {
+        if (err) return res.sendStatus(403.).send('Invalid token.');
+
+        //check if user has permission
+        if(req.params.cId != undefined && req.params.cId != userData.Company) return res.sendStatus(401);
+        if(req.params.uId != undefined && req.params.uId != userData.EmployeeNumber) return res.sendStatus(401);
+        if(req.params.email != undefined && req.params.uId != userData.EMail) return res.sendStatus(401);
         next();
     });
 }
