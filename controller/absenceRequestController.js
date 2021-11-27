05449cc8cf8cb35280eed5889b0011fbaf73acab
Philipp Uthoff <philipp.uthoff@gmx.de>
2021-11-27 15:17:28 +0100

    Bug fix

--- controller/absenceRequestController.js
+++ controller/absenceRequestController.js
@@ -24,11 +24,11 @@ module.exports.getHolidays = async function (req, res) {
     const endDate = req.params.end;
     let year = startDate.substring(0, 4);
 
-    let prevYear = getPrevYear(cId, uId, year);
-    let thisYear = getThisYear(cId, uId, year);
-    let corrections = getCorrections(cId, uId, year);
-    let taken = getTaken(cId, uId, year);
-    let thisRequest = getThisRequest(cId, uId, startDate, endDate);
+    let prevYear = await getPrevYear(cId, uId, year);
+    let thisYear = await getThisYear(uId);
+    let corrections = await getCorrections(cId, uId, year);
+    let taken = (await getTaken(cId, uId, year)) * -1;
+    let thisRequest = (await getThisRequest(startDate, endDate)) * -1;
     let total = await prevYear + await thisYear;
     let open = total + await corrections + await taken;
     let remaining = open + await thisRequest;
@@ -36,11 +36,11 @@ module.exports.getHolidays = async function (req, res) {
     res.send({
         'prev': prevYear,
         'this': thisYear,
+        'total': total,
         'corrections': corrections,
         'taken': taken,
-        'thisRequest': thisRequest,
-        'total': total,
         'open': open,
+        'thisRequest': thisRequest,
         'remaining': remaining
     });
 }
@@ -50,34 +50,34 @@ async function getPrevYear(cId, uId, year) {
 
     const query = `SELECT HolidayStart FROM B_Salery WHERE EmployeeNumber = ${uId} AND Company = ${cId} AND Year = ${year} AND HolidayStart IS NOT NULL;`;
     const result = await db.query(query);
-    if (result[0] !== undefined) return result.HolidayStart;
+    if (result[0] !== undefined) return result[0].HolidayStart;
     else return 0;
 }
 
-async function getThisYear(cId) {
-    const query = `SELECT HolidayPerYear FROM B_Employees WHERE EmployeeNumber = ${cId}`;
+async function getThisYear(uId) {
+    const query = `SELECT HolidayPerYear FROM B_Employees WHERE EmployeeNumber = ${uId}`;
     const result = await db.query(query);
-    if (result[0] !== undefined) return result.HolidayPerYear;
+    if (result[0] !== undefined) return result[0].HolidayPerYear;
     else return 0;
 }
 
 async function getCorrections(cId, uId, year) {
     const query = `SELECT HolidayAdjustment FROM S_TimeAdjustment WHERE EmployeeNumber = ${uId} AND Company = ${cId} AND Year = ${year} AND HolidayAdjustment IS NOT NULL;`;
     const result = await db.query(query);
-    if (result[0] !== undefined) return result.HolidayAdjustment;
+    if (result[0] !== undefined) return result[0].HolidayAdjustment;
     else return 0;
 }
 
 async function getTaken(cId, uId, year) {
     const query = `SELECT sum(DaysHoliday) as holidays FROM DCM.dbo.S_WeekPlan WHERE EmployeeNumber = ${uId} AND Company = ${cId} AND Year = ${year};`;
     const result = await db.query(query);
-    if (result[0] !== undefined) return result.holidays;
+    if (result[0] !== undefined) return result[0].holidays;
     else return 0;
 }
 
 async function getThisRequest(start, end) {
     const query = `SELECT COUNT(Date) as Count FROM B_Calendar WHERE Date >= '${start}' AND Date <= '${end}' AND (KindOfDay = 0 OR KindOfDay = 4 OR KindOfDay = 7)`;
     const result = await db.query(query);
-    if (result[0] !== undefined) return result.Count;
+    if (result[0] !== undefined) return result[0].Count;
     else return 0;
 }
